482f5a257b452fa9bbfbc240351ac3d083ab57dc
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2019-12-06 03:09:53 +0100

    Revert "Merge branch 'simulation' of https://github.com/CheesyTeam-NDI/project into simulation"
    This reverts commit 4584af83b50614622e763a50c3bb74e3c8337f01, reversing
    changes made to b889a75265bd8abaec25a8db4a0e3ab4b03e4b70.

--- src/app/_components/simulation/simulation.component.ts
+++ src/app/_components/simulation/simulation.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-simulation',
   templateUrl: './simulation.component.html',
-  styleUrls: ['./simulation.component.scss']
+  styleUrls: ['./simulation.component.css']
 })
 export class SimulationComponent implements OnInit {
 
