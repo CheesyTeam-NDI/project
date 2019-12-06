4816510bbe22d7d36a789e66dbcebe52a130ae93
Mathis <mathisengels@yahoo.fr>
2019-12-06 06:32:11 +0100

    create the first two pages ( homepage and about )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './_components/about/about.component';
-import { FooterComponent } from './_components/general/footer/footer.component';
+import {AppComponent} from './app.component';
+import {AboutComponent} from './_components/about/about.component';
+import {FooterComponent} from './_components/general/footer/footer.component';
 import {HeaderComponent} from './_components/general/header/header.component';
 import {NavbarComponent} from './_components/general/navbar/navbar.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {AppRoutingModule} from './app-routing.module';
-import { HomepageComponent } from './_components/homepage/homepage.component';
+import {HomepageComponent} from './_components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
@@ -27,4 +27,5 @@ import { HomepageComponent } from './_components/homepage/homepage.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
