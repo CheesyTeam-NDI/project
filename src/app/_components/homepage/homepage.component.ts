dd32fc2f6a3ebc4de28d00a4b4cf540747bf832e
Thomas <t.bauduin7@laposte.net>
2019-12-06 05:21:13 +0100

    Co-authored-by: Lilian Delouvy <lilian.delouvy@gmail.com> Co-authored-by: Antoine Balieu <Vhayn@users.noreply.github.com>

--- src/app/_components/homepage/homepage.component.ts
+++ src/app/_components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Aide } from 'src/app/_models/aides';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  aides: Aide[];
+  constructor() { 
+    
+}
+
+
 
   ngOnInit() {
+    this.aides = [new Aide("Le Crous","Le CROUS est un établissement public ayant pour mission d'améliorer les conditions de vie et de travail des étudiants.",1),
+                  new Aide("La CAF","La CAF peut fournir des aides au logement aux étudiants.",2),
+                  new Aide("Les bourses","Il est également possible de demander des bourses selon votre situation sociale, si vous êtes un étudiant ERASMUS...",3),
+                  new Aide("La Mission Locale","La mission locale oriente et suit le parcours des anciens étudiants dans un but d'insertion professionnelle et sociale.",4),
+                  new Aide("L'Allocation logement","Action Logement peut fournir une subvention allégeant la quittance loyer.",5)];
+    
   }
 
 }
