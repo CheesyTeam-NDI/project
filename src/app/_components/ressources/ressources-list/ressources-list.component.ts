6c735d4ccef44b3f7e4f1c0540e47cf58a3152ef
Mathis <mathisengels@yahoo.fr>
2019-12-06 05:47:15 +0100

    add ressources list component

---
+++ src/app/_components/ressources/ressources-list/ressources-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ressources-list',
+  templateUrl: './ressources-list.component.html',
+  styleUrls: ['./ressources-list.component.scss']
+})
+export class RessourcesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RessourcesDetailsComponent } from './_components/ressources/ressources-details/ressources-details.component';
+import { RessourcesListComponent } from './_components/ressources/ressources-list/ressources-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RessourcesDetailsComponent
+    RessourcesDetailsComponent,
+    RessourcesListComponent
   ],
   imports: [
     BrowserModule
