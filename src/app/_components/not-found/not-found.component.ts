d1b222af6822a9e591723a808234b4a1a1649233
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2019-12-06 05:57:03 +0100

    carre rouge

--- src/app/_components/not-found/not-found.component.ts
+++ src/app/_components/not-found/not-found.component.ts
@@ -1,5 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+function myMove() {
+  var url = '';
+  var elem = document.getElementById("myAnimation");
+  var bt1 = document.getElementById("bt-contact");
+  var bt2 = document.getElementById("bt-autre");
+  var msg = document.getElementById("affichageUrl");
+  var lienURL = document.getElementById("lien");
+
+
+  bt1.style.display = "none";
+  bt2.style.display = "none";
+  msg.style.display = "none";
+  lienURL.style.display = "none";
+
+
+
+  var pos = -200;
+  elem.style.bottom = '5px';
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos > 200) {
+      // et qu'es tu fais la ? tu cherches ça ?
+      bt1.style.display = "block";
+      bt2.style.display = "block";
+      msg.style.display = "block";
+      lienURL.style.display = "block";
+      clearInterval(id);
+    } else {
+      pos = pos + 3;
+      elem.style.left = pos + 'px';
+    }
+
+  }
+
+}
+
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
@@ -9,7 +45,11 @@ export class NotFoundComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  name = 'Angular';
 
+  ngOnInit() {
+    myMove()
+  }
 }
+
+
