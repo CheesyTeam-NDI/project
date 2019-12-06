5e953f8e0c1d4da7bd8f00cf7110fbaf0431a160
Mathis <mathisengels@yahoo.fr>
2019-12-06 07:45:21 +0100

    add ressources details, missing ressource list

--- src/app/_components/ressources/ressources-details/ressources-details.component.ts
+++ src/app/_components/ressources/ressources-details/ressources-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-ressources-details',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RessourcesDetailsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
   }
 
+  id: string;
+
+  // Variables
+
+  description: string;
+  resourceTitle: string;
+  image: number;
+  contactTitle: string;
+  geolocalisation: string;
+  email: string;
+  tel: string;
+  adresse: string;
+  nom: string;
+
+  // Initialisation des variables
+  ngOnInit() {
+    this.resourceTitle = 'Titre';
+    this.description = 'Description';
+    this.geolocalisation = 'Zone Geolocalisation';
+    this.image = 1;
+    this.nom = 'Nom';
+    this.email = 'contact@contact.fr';
+    this.tel = '001122334455';
+    this.adresse = '1 rue de Paris';
+
+    this.route.paramMap.subscribe(value => {
+      this.id = value.get('id');
+    });
+
+    // Based on the ID, get from the server and override the values
+
+  }
 }
+
